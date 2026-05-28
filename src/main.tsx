2ff0ac52175374d1a18854c7d95adde8c841b188
Jayvee <jayvee.github0703@gmail.com>
2026-05-28 10:31:36 +0800

    Github initialized

---
+++ src/main.tsx
@@ -0,0 +1,18 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import App from './App'
+
+
+const routes = createBrowserRouter([
+  {
+    path:'/',
+    element:<App/>,
+  }
+])
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <RouterProvider router={routes}/>
+  </StrictMode>,
+)
